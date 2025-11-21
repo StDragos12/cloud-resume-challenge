871b2e782ea226a238889686463758084dcad690
StDragos12 <stirbandragos@yahoo.ro>
2025-11-21 15:19:13 +0200

    xcade

--- cloud-resume-backend/function_app.py
+++ cloud-resume-backend/function_app.py
@@ -31,7 +31,7 @@ def _get_container():
 
 @app.function_name(name="views")
 @app.route(
-    route="/api/views",
+    route="views",
     methods=["GET", "POST"],
     auth_level=func.AuthLevel.ANONYMOUS,  
 )
